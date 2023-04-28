080acf40d50b266f23f738502be63dffa02c4d9c
alissonbatalha <82676146+alissonbatalha@users.noreply.github.com>
2023-04-28 15:14:26 +0000

    Formato mobile adicionado / sistema de pontuação adicionada

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-const mario = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+let placar = document.querySelector(".placar");
 
 const jump = () => {
-  mario.classList.add('jump');
+  mario.classList.add("jump");
 
   setTimeout(() => {
-   mario.classList.remove('jump');
+    mario.classList.remove("jump");
   }, 500);
 };
 
 const loop = setInterval(() => {
-   
   const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+  const marioPositionX = mario.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
 
-  if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-  
-    pipe.style.animation = 'none';
+  if (pipePosition <= 60 && pipePosition > 0 && marioPosition < 40) {
+    pipe.style.animation = "none";
     pipe.style.left = `${pipePosition}px`;
 
-    mario.style.animation = 'none';
+    mario.style.animation = "none";
     mario.style.bottom = `${marioPosition}px`;
 
-    mario.src = './images/game-over.png';
-    mario.style.width = '75px';
-    mario.style.marginLeft = '50px';
+    mario.src = "./images/game-over.png";
+    mario.style.width = "40px";
+    mario.style.marginLeft = "25px";
 
     clearInterval(loop);
-  
   }
+}, 5);
 
-}, 10);
+const pontua = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPositionX = mario.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
+
+  if (marioPositionX > pipePosition) {
+    placar.innerText++;
+  }
+}, 500);
+
+document.addEventListener("keydown", jump);
 
 
-document.addEventListener('keydown', jump);
